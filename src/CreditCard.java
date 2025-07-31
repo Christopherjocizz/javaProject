b5f04bba0bd079fd5809c8fa7b4daf01844aad53
Christopher <christopherjocizz@gmail.com>
2025-07-31 00:15:27 -0500

    updatePublicClassJavaCreditCard.java

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,4 +1,9 @@
 public class CreditCard {
 //privateAttributes
     private String cardNumber;
+    private String titularName;
+    private String expirationDate;
+    private String cvv;
+    private double creditLine;
+    private double availableBalance;
 }
