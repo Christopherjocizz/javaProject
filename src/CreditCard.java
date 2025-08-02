eb457f1168e755778ffa99ed8b659cec014c21ec
Christopher <christopherjocizz@gmail.com>
2025-08-02 17:26:29 -0500

    updatePublicClassJavaCreditCard.java

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -10,4 +10,16 @@ public class CreditCard {
     private double availableBalance;
     private boolean active;
     private List<shopping> shoppingList;
+
+//constructor
+
+    public CreditCard(String cardNumber, String titularName, String expirationDate, String cvv, double creditLine, double availableBalance, boolean active) {
+        this.cardNumber = cardNumber;
+        this.titularName = titularName;
+        this.expirationDate = expirationDate;
+        this.cvv = cvv;
+        this.creditLine = creditLine;
+        this.availableBalance = availableBalance;
+        this.active = active;
+    }
 }
